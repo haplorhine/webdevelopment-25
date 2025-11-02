5f3f88c63aee524598fe69b8c79d31e2f5aa8e0d
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2025-11-02 17:37:01 +0100

    Add daisyui

---
+++ event-app-frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Hero from './components/organisms/Hero.vue'
+</script>
+
+<template>
+  <Hero data-theme="valentine" />
+  <Hero data-theme="cupcake" />
+  <Hero data-theme="synthwave" />
+</template>
+
+<style scoped></style>
