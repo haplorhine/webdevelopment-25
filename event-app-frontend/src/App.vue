c18c11825f8703d827f29633e7fecb295b16717f
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2025-11-02 18:25:55 +0100

    Change main template

--- event-app-frontend/src/App.vue
+++ event-app-frontend/src/App.vue
@@ -3,9 +3,7 @@ import Hero from './components/organisms/Hero.vue'
 </script>
 
 <template>
-  <Hero data-theme="valentine" />
   <Hero data-theme="cupcake" />
-  <Hero data-theme="synthwave" />
 </template>
 
 <style scoped></style>
