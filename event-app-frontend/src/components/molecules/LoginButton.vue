271258d70cd12fd33c56659cda503b6c28217f61
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2025-11-04 16:32:11 +0100

    Create Login Button

---
+++ event-app-frontend/src/components/molecules/LoginButton.vue
@@ -0,0 +1,9 @@
+<script setup>
+import UserIcon from '../atoms/UserIcon.vue'
+</script>
+<template>
+  <button class="btn bg-white text-black border-[#e5e5e5]">
+    <UserIcon />
+    Login
+  </button>
+</template>
