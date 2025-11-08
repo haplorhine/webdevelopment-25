12600be79fc079c78c535c5142e9a3219503e9bc
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2025-11-08 22:02:21 +0100

    add Mapping

--- event-app-backend/src/main/java/at/technikum/springrestbackend/mapper/EventMapper.java
+++ event-app-backend/src/main/java/at/technikum/springrestbackend/mapper/EventMapper.java
@@ -3,9 +3,11 @@ package at.technikum.springrestbackend.mapper;
 import at.technikum.springrestbackend.dto.EventDto;
 import at.technikum.springrestbackend.entity.EventEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface EventMapper {
+    @Mapping(target = "hostId", source = "host.id")
     EventDto toDto(EventEntity eventEntity);
     EventEntity toEntity(EventDto dto);
 }
