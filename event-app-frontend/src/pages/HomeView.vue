00ba06d1b6247a55af9d4147883648185e77905d
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2025-11-10 10:40:36 +0100

    Change welcome text

--- event-app-frontend/src/pages/HomeView.vue
+++ event-app-frontend/src/pages/HomeView.vue
@@ -3,7 +3,7 @@ import TheHero from '@/components/organisms/TheHero.vue'
 
 const heading = 'This is the best event page ever!'
 const text =
-  'Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.'
+  'Hello Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.'
 </script>
 
 <template>
