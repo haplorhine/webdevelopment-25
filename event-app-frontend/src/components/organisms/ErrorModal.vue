b1e486de0c05adaca651a4e09650af36984c03e0
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2025-11-18 15:20:52 +0100

    Create error modal component

---
+++ event-app-frontend/src/components/organisms/ErrorModal.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps({
+  show: {
+    type: Boolean,
+    default: false,
+  },
+})
+</script>
+
+<template>
+  <dialog v-if="props.show" class="modal" open>
+    <div class="modal-box">
+      <slot />
+      <div class="modal-action">
+        <form method="dialog">
+          <button class="btn">close</button>
+        </form>
+      </div>
+    </div>
+  </dialog>
+</template>
