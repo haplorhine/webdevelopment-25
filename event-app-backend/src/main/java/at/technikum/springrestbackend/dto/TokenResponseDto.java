0352dd8ce1b446d93689ada20b4c30bb89f35641
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2025-12-09 16:27:13 +0100

    Add lombok builder to work with dummy authservice

--- event-app-backend/src/main/java/at/technikum/springrestbackend/dto/TokenResponseDto.java
+++ event-app-backend/src/main/java/at/technikum/springrestbackend/dto/TokenResponseDto.java
@@ -1,14 +1,12 @@
 package at.technikum.springrestbackend.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class TokenResponseDto {
     private String token;
 }
