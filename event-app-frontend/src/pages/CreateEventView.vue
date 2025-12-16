0f0ad12839aee82efb4d08f0f1ae67df39bccda8
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2025-12-16 18:34:08 +0100

    Add check

--- event-app-frontend/src/pages/CreateEventView.vue
+++ event-app-frontend/src/pages/CreateEventView.vue
@@ -1,5 +1,18 @@
 <script setup>
+import { useRouter } from 'vue-router'
+import { onMounted } from 'vue'
+import { useUserStore } from '@/state/user'
 import CreateEventHero from '@/components/organisms/CreateEventHero.vue'
+
+const router = useRouter()
+const userStore = useUserStore()
+
+onMounted(() => {
+  userStore.loadFromStorage()
+  if (userStore.role !== 'HOST') {
+    router.replace('/')
+  }
+})
 </script>
 
 <template>
