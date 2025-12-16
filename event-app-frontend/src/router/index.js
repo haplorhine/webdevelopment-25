2a0082cb8e9b0f28e7411946087b3b2d7609f9b3
annetlknn <238889630+annetlknn@users.noreply.github.com>
2025-12-16 08:57:27 +0100

    New index.js

--- event-app-frontend/src/router/index.js
+++ event-app-frontend/src/router/index.js
@@ -1,28 +1,37 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
-import HomeView from '@/pages/HomeView.vue'
-import EventView from '@/pages/EventView.vue'
+import HomeView from "@/pages/HomeView.vue";
+import EventView from "@/pages/EventView.vue";
 
-// lazy load other routes
-const AboutView = () => import('@/pages/AboutView.vue')
-const LoginView = () => import('@/pages/LoginView.vue')
-const RegisterView = () => import('@/pages/RegisterView.vue')
-const ImprintView = () => import('@/pages/ImprintView.vue')
-const HelpView = () => import('@/pages/HelpView.vue')
+
+const AboutView = () => import("@/pages/AboutView.vue");
+const LoginView = () => import("@/pages/LoginView.vue");
+const RegisterView = () => import("@/pages/RegisterView.vue");
+const ImprintView = () => import("@/pages/ImprintView.vue");
+const HelpView = () => import("@/pages/HelpView.vue");
+
+
+const ForgotPasswordView = () => import("@/pages/ForgotPasswordView.vue");
 
 const routes = [
-  { path: '/', component: HomeView },
-  { path: '/events', component: EventView },
-  { path: '/about', component: AboutView },
-  { path: '/imprint', component: ImprintView},
-  { path: '/help', component: HelpView},
-  { path: '/login', component: LoginView },
-  { path: '/register', component: RegisterView },
-]
+  { path: "/", component: HomeView },
+  { path: "/events", component: EventView },
+  { path: "/about", component: AboutView },
+  { path: "/imprint", component: ImprintView },
+  { path: "/help", component: HelpView },
+  { path: "/login", component: LoginView },
+  { path: "/register", component: RegisterView },
+
+
+  { path: "/forgot-password", component: ForgotPasswordView },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-})
+});
 
-export default router
+export default router;
+
+
+
