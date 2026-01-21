8cf7585684c4c1b17b4e01849fcf98f474202eae
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2026-01-21 17:40:30 +0100

    Add ImageService

---
+++ event-app-backend/src/main/java/at/technikum/springrestbackend/services/ImageService.java
@@ -0,0 +1,48 @@
+package at.technikum.springrestbackend.services;
+
+import at.technikum.springrestbackend.dto.ImageDto;
+import at.technikum.springrestbackend.entity.ImageEntity;
+import at.technikum.springrestbackend.repositories.ImageRepository;
+import at.technikum.springrestbackend.storage.FileStorage;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class ImageService {
+    private final ImageRepository imageRepository;
+
+    private final ImageMapper imageMapper;
+
+    private final FileStorage fileStorage;
+
+    public ImageDto upload(MultipartFile toUpload) {
+        String externalId = fileStorage.upload(toUpload);
+
+        ImageEntity image = new ImageEntity();
+        image.setName(toUpload.getOriginalFilename());
+        image.setExternalId(externalId);
+        image.setContentType(toUpload.getContentType());
+
+        return imageMapper.toDto(imageRepository.save(image));
+    }
+
+    public ImageEntity findById(UUID id) {
+        return imageRepository.findById(id)
+                .orElseThrow(EntityNotFoundException::new);
+    }
+
+    public Resource asResource(ImageEntity image) {
+        InputStream stream = fileStorage.load(image.getExternalId());
+
+        return new InputStreamResource(stream);
+    }
+
+}
