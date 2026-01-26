52485bdd368622b8b0d600431b185a3f5a884a51
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2026-01-26 14:45:29 +0100

    Update nav links

--- event-app-frontend/src/components/organisms/TheNavbar.vue
+++ event-app-frontend/src/components/organisms/TheNavbar.vue
@@ -22,12 +22,12 @@ const getImageUrl = (imageId) => {
 }
 
 const displayedLinks = computed(() => {
-  if (userStore.role === 'ADMIN') {
+  if (userStore.isAdmin) {
     return [
       { to: '/events', label: 'Events' },
+      { to: '/event-management', label: 'Event Management' },
       { to: '/user-management', label: 'User Management' },
       { to: '/ticket-management', label: 'Ticket Management' },
-
     ]
   }
 
@@ -37,7 +37,8 @@ const displayedLinks = computed(() => {
     currentLinks = currentLinks.filter((link) => link.label !== 'Register')
   }
 
-  if (userStore.role === 'HOST') {
+  if (userStore.isHost) {
+    currentLinks.push({ to: '/event-management', label: 'Event Management' })
     currentLinks.push({ to: '/create-event', label: 'Create Event' })
   }
 
@@ -58,11 +59,25 @@ onMounted(() => {
     <div class="navbar-start">
       <div class="dropdown">
         <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h8m-8 6h16"
+            />
           </svg>
         </div>
-        <ul tabindex="-1" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <ul
+          tabindex="-1"
+          class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+        >
           <li v-for="link in displayedLinks" :key="link.to">
             <RouterLink :to="link.to">{{ link.label }}</RouterLink>
           </li>
@@ -81,13 +96,23 @@ onMounted(() => {
       <div v-if="isLoggedIn" class="dropdown dropdown-end">
         <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
           <div class="w-10 rounded-full">
-            <img v-if="userStore.imageId" :src="getImageUrl(userStore.imageId)" alt="User Profile"
-              class="object-cover w-full h-full" />
-            <img v-else alt="Tailwind CSS Navbar component"
-              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            <img
+              v-if="userStore.imageId"
+              :src="getImageUrl(userStore.imageId)"
+              alt="User Profile"
+              class="object-cover w-full h-full"
+            />
+            <img
+              v-else
+              alt="Tailwind CSS Navbar component"
+              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+            />
           </div>
         </div>
-        <ul tabindex="-1" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <ul
+          tabindex="-1"
+          class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+        >
           <li>
             <RouterLink to="/profile" class="justify-between">
               Profile
@@ -95,9 +120,7 @@ onMounted(() => {
             </RouterLink>
           </li>
           <li>
-            <RouterLink to="/my-tickets" class="justify-between">
-              My Tickets
-            </RouterLink>
+            <RouterLink to="/my-tickets" class="justify-between"> My Tickets </RouterLink>
           </li>
           <li><a @click="logout">Logout</a></li>
         </ul>
